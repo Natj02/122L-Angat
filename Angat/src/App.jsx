de917ad7cf7bdfc127788a4f8553d7feea21398f
wolz4k <glennchristianegopez@gmail.com>
2025-02-22 23:30:52 +0800

    Updated DaisyUI version

--- Angat/src/App.jsx
+++ Angat/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="w-full">
+      <div class="dropdown dropdown-start">
+  <div tabindex="0" role="button" class="btn m-1">Click ⬇️</div>
+  <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
+      </div>
     </>
   )
 }
