1093625aa50e0b7ae98ec6a7f9a1f4c2748c41a1
wolz4k <glennchristianegopez@gmail.com>
2025-02-23 02:05:03 +0800

    Added navbar

--- Angat/src/App.jsx
+++ Angat/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import angatLogo from './assets/img/logo.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
@@ -9,13 +9,18 @@ function App() {
   return (
     <>
       <div className="w-full">
-      <div class="dropdown dropdown-start">
-  <div tabindex="0" role="button" class="btn m-1">Click ⬇️</div>
-  <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    <li><a>Item 1</a></li>
-    <li><a>Item 2</a></li>
-  </ul>
-</div>
+        <div class="navbar bg-base-100 shadow-sm ">
+          <div class="flex-1">
+            <a class="btn btn-ghost text-xl h-16 w-16 p-0">
+              <img src={angatLogo}  alt="Angat Logo"/>
+            </a>
+          </div>
+          <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
+            </button>
+          </div>
+        </div>
       </div>
     </>
   )
