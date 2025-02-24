296a4d0e32a942c0b0d1200f8ca541f4982d9d25
wolz4k <glennchristianegopez@gmail.com>
2025-02-25 00:40:38 +0800

    Created AboutUs and initial Projects.jsx

--- Angat/src/frontend/AboutUs.jsx
+++ Angat/src/frontend/AboutUs.jsx
@@ -4,9 +4,8 @@ import banner from '../assets/img/banner.png'
 import './components/header'
 import Header from './components/header'
 import Footer from './components/footer'
-// import './App.css'
 
-function Landing() {
+function AboutUs() {
   const [count, setCount] = useState(0)
 
   return (
@@ -15,21 +14,101 @@ function Landing() {
         {/* Navbar */}
         <Header />
         {/* Hero Banner */}
-        <div
-          class="hero min-h-160 justify-items-start items-end "
-          style={{
-            backgroundImage: `url(${banner})`,
-          }}>
-          <div class="hero-overlay"></div>
-          <div class="hero-content text-base-100">
-            <div class="max-w-2xl">
-              <h1 class="mb-5 text-8xl font-bold ">Angat Cembo</h1>
-              <p class="mb-5">
-                insert info
-              </p>
+        <div className="flex flex-row">
+          <div
+            class="hero w-1/3 min-h-160 justify-items-start items-end "
+            style={{
+              backgroundImage: `url(${banner})`,
+            }}>
+            <div class="hero-overlay"></div>
+            <div class="hero-content text-base-100">
+              <div class="max-w-2xl">
+                <h1 class="mb-5 text-8xl font-bold ">Angat Cembo</h1>
+                <p class="mb-5">
+                  insert info
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="hero w-1/3 min-h-160 justify-items-start items-end "
+            style={{
+              backgroundImage: `url(${banner})`,
+            }}>
+            <div class="hero-overlay"></div>
+            <div class="hero-content text-base-100">
+              <div class="max-w-2xl">
+                <h1 class="mb-5 text-8xl font-bold ">Angat Cembo</h1>
+                <p class="mb-5">
+                  insert info
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="hero w-1/3 min-h-160 justify-items-start items-end "
+            style={{
+              backgroundImage: `url(${banner})`,
+            }}>
+            <div class="hero-overlay"></div>
+            <div class="hero-content text-base-100">
+              <div class="max-w-2xl">
+                <h1 class="mb-5 text-8xl font-bold ">Angat Cembo</h1>
+                <p class="mb-5">
+                  insert info
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        
+        <div className='w-full p-14 bg-base-200 shadow-sm'>
+          <div className="m-auto w-4xl">
+            <div className="text-lg text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et ullamcorper ipsum. Morbi fermentum consectetur vestibulum. Etiam elementum porttitor mi eu rutrum. Duis ut varius ex, at egestas augue. Aenean dui ligula, mattis a mi eget, egestas dictum velit. Aliquam a arcu nec tortor imperdiet efficitur. Aenean ullamcorper nibh pharetra, hendrerit eros sit amet, venenatis sem. Integer laoreet nulla sem, vel vestibulum justo elementum quis. Suspendisse at molestie est, elementum aliquet est. Aenean enim lectus, maximus quis libero in, sagittis vulputate ante. Nulla pulvinar interdum tortor pulvinar dapibus. Nullam commodo hendrerit luctus. Suspendisse lacinia ligula ultricies tortor mollis maximus. Morbi ultrices lectus sit amet risus pretium, ac dignissim justo molestie.
+            </div>
+           
+          </div>
+        </div>
+
+        <div className='w-full p-14 bg-secondary shadow-sm'>
+          <div className="m-auto w-4xl">
+            <div className='flex flex-row justify-between'>
+              <div>
+                <div className='text-2xl font-bold'>
+                  Our Vision
+                </div>
+                <div className='text-3xl font-bold mt-6 max-w-2xl'>
+                  j;slfkjds;kljf
+                </div>
+              </div>
+              
+              <div>
+                <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="m-auto w-4xl">
+            <div className='flex flex-row justify-between'>
+              <div>
+                <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" alt="" />
+              </div>
+              <div>
+                <div className='text-2xl font-bold'>
+                  Our Vision
+                </div>
+                <div className='text-3xl font-bold mt-6 max-w-2xl'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et ullamcorper ipsum. Morbi fermentum consectetur vestibulum. Etiam elementum porttitor mi eu rutrum. Duis ut varius ex, at egestas augue. Aenean dui ligula, mattis a mi eget, egestas dictum velit. Aliquam a arcu nec tortor imperdiet efficitur. 
+                </div>
+              </div>
+              
+              
+            </div>
+          </div>
+        </div>
+
+          
         
 
         <Footer />
@@ -41,4 +120,4 @@ function Landing() {
   )
 }
 
-export default Landing
+export default AboutUs
--- Angat/src/frontend/main.jsx
+++ Angat/src/frontend/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import Landing from './Landing.jsx'
 import Register from './register.jsx'
+import AboutUs from './AboutUs.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
