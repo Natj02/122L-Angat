b0d3d423f022e377be78b4cf18ec614f8356cd6c
andyrews <126462930+andyrews@users.noreply.github.com>
2025-03-13 16:59:49 +0800

    Fixed Confirm Email

--- Angat/src/frontend/components/ConfirmEmailLanding.jsx
+++ Angat/src/frontend/components/ConfirmEmailLanding.jsx
@@ -27,40 +27,52 @@ const ConfirmEmailLanding = () => {
 
   return (
     <>
-        <div className="overflow-x-hidden overflow-y-auto m-10">
-          <div className=" flex  flex-col items-center">
-            <div className="w-24 h-24">
-              <img src={angatLogo} alt="Angat Logo" />
-            </div>
-            <h1 className="font-bold my-10 text-3xl">
-              Erm... Actually, please verify your email
-            </h1>
-            <div className="w-96 h-96 m-0">
-              <img src={erm_actually} alt="Angat Logo" />
-            </div>
-            <div className="px-32">
-              <h3 className="text-lg text-wrap text-justify">
-                Check your inbox at <b>{email}</b> and click the verification
-                link inside to complete your registration. This link is set to
-                expire shortly, so verify soon!
-              </h3>
+      <div className="overflow-x-hidden overflow-y-auto m-10">
+        <div className=" flex  flex-col items-center">
+          <div className="w-24 h-24">
+            <img src={angatLogo} alt="Angat Logo" />
+          </div>
+          <h1 className="font-bold my-10 text-3xl">
+            Please verify your email!
+          </h1>
+          <div className="border-8 border-red-500 flex justify-center mb-6">
+            <svg
+              class="h-52 w-52 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1"
+                d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76"
+              />
+            </svg>
+          </div>
+          <div className="px-32">
+            <h3 className="text-lg text-wrap text-justify">
+              Check your inbox at <b>{email}</b> and click the verification link
+              inside to complete your registration. This link is set to expire
+              shortly, so verify soon!
+            </h3>
 
-              <h3 className="mt-10 text-lg">
-                <b>Don't see an email? </b> Check your spam folder.
-              </h3>
-              <h3 className="mt-10 text-lg">
-                <b>Link Expired? </b>{" "}
-                <button
-                  className="text-blue-700 cursor-pointer hover:underline"
-                  onClick={handleResendEmail}
-                >
-                  Resend verficiation email
-                </button>
-                {message && <p className="text-sm">{`(${message})`}</p>}
-              </h3>
-            </div>
+            <h3 className="mt-10 text-lg">
+              <b>Don't see an email? </b> Check your spam folder.
+            </h3>
+            <h3 className="mt-10 text-lg">
+              <b>Link Expired? </b>{" "}
+              <button
+                className="text-blue-700 cursor-pointer hover:underline"
+                onClick={handleResendEmail}
+              >
+                Resend verficiation email
+              </button>
+              {message && <p className="text-sm">{`(${message})`}</p>}
+            </h3>
           </div>
         </div>
+      </div>
     </>
   );
 };
