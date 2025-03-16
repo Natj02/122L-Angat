8accb9bbebb5f1a47664ebe57511ab619912f21a
wolz4k <glennchristianegopez@gmail.com>
2025-03-16 18:00:24 +0800

    overflow hidden and ellipsis for search results

--- Angat/src/frontend/components/header.jsx
+++ Angat/src/frontend/components/header.jsx
@@ -148,7 +148,7 @@ function Header() {
                   >
                  <div class>
                   <div className="font-bold text-md text-nowrap truncate">{item.name}</div>
-                  <div className="text-xs truncate max-h-24">{item.description}</div>
+                  <div className="text-xs truncate max-h-24 overflow-hidden text-ellipsis">{item.description}</div>
                   <div className="text-xs">{item.type}</div>
                  </div>
                 </ Link>
